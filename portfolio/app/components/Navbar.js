f47563fff221f794b5dace87e56ead2a6524fd43
J-Tajheria <jordantajheria@gmail.com>
2025-05-14 17:44:00 +0100

    Adding more details to navbar

--- portfolio/app/components/Navbar.js
+++ portfolio/app/components/Navbar.js
@@ -6,28 +6,27 @@ const Navbar = () => {
   return (
     <>
     {/* Define different breakpoints for different screen sizes */}
-        <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex justify-between items-center z-50'>
+        <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
             <a href="#top">
                 <Image src={assets.logo} alt="logo" className='w-28 cursor-pointer mr-14'/>
             </a>
-            <ul className='hidden md:flex items-center gap-6 lg:gap-8 xl:gap-10'>
+            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
                 <li>
-                    <a href="#top">Home</a>
+                    <a className='font-Ovo' href="#top">Home</a>
                 </li>
                 <li>
-                    <a href="#about">About Me</a>
+                    <a className='font-Ovo' href="#about">About Me</a>
                 </li>
                 <li>
-                    <a href="#projects">My Projects</a>
+                    <a className='font-Ovo' href="#projects">My Projects</a>
                 </li>
                 <li>
-                    <a href="#contact">Contact Me</a>
+                    <a className='font-Ovo' href="#contact">Contact Me</a>
                 </li>
-                
             </ul>
             <div>
-                <a href="#contact" >Contact<Image src={assets.arrow_icon} alt="logo" className='w-3 '/></a>
-            </div>
+                <a className=' font-Ovo hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4' href="#contact" >Contact<Image src={assets.arrow_icon} alt="logo" className='w-3 '/></a>
+            </div> 
         </nav>
     </>
   )
